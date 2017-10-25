b12e4a25ac0ac0c3d5a98d547d18a268329f1965
kitihounel <kitihounel@gmail.com>
2017-10-25 08:19:53 +0100

    Edited readme

--- icpc/south-africa/2014/amusementpark.cpp
+++ icpc/south-africa/2014/amusementpark.cpp
@@ -1,7 +1,7 @@
 /**
  * Simulation problem.
  * The solution is to simulate the arrival of the customers at the rides and
- * to count the number of free rides that will be given.
+ * to count the number of free rides that will be given per customer.
  * Note that there will be at least N free rides given.
  */
 #include <iostream>
@@ -13,31 +13,35 @@ int main()
 {
     int  a, n;
     long long int freeRideCount;
+    vector<int> v;
 
-    while(cin >> n && n != -1) {
-        vector<int> next(n);
-        for(int i = 0; i < n; i++) {
+    v.reserve(150000ul);
+
+    while (cin >> n && n != -1) {
+
+        for (int j = 0; j < n; ++j) {
             cin >> a;
-            next[i] = a - 1;
+            v.push_back(a - 1);
         }
 
         freeRideCount = n;
-        for(int i = 0; i < n; i++) {
+        for (int j = 0; j < n; ++j) {
             vector<bool> takenRides(n, false);
             int currentRide, nextRide;
 
-            currentRide = i;
-            nextRide    = next[currentRide];
+            currentRide = j;
+            nextRide = v[currentRide];
             takenRides[currentRide] = true;
             while (!takenRides[nextRide]) {
                 ++freeRideCount;
                 takenRides[nextRide] = true;
                 currentRide = nextRide;
-                nextRide    = next[currentRide];
+                nextRide = v[currentRide];
             }
         }
 
         cout << freeRideCount << endl;
+        v.clear();
     }
 
     return 0;
---
+++ icpc/south-africa/2015/tallorders.cpp
@@ -0,0 +1,69 @@
+/**
+ * This problem can be solved using Newton-Raphson method to find the zero
+ * of a function.
+ * The solution implemented here is described in an article of the
+ * Undergraduate Journal of Mathematical Modeling: One + Two. The article is
+ * contained in the file `article.pdf`. We advice to read the article before
+ * diving in the code below.
+ */
+#include <iostream>
+#include <cstdio>
+#include <cmath>
+
+using namespace std;
+
+long double f(long double a, long double d, long double p)
+{
+    long double v = d / (2.0l * a);
+    long double s = p - 4.2l;
+
+    return (a + s - cosh(v) * a);
+}
+
+long double g(long double a, long double d)
+{
+    long double v = d / (2.0l * a);
+
+    return (1.0l - cosh(v) + v * sinh(v));
+}
+
+/*
+ * This function computes the value of the catenary parameter `a`, given `p`
+ * and `d`, with an error of 0.00001 using the Newton-Raphson method.
+ */
+long double newtonRaphson(long double d, long double p)
+{
+    long double a, b, eps;
+
+    /* In the document where we found the solution, a is set to 20 ft.  */
+    /* 1 ft = 0.304 m and 20 ft = 6.08 m. But we choose to set a = 10.  */
+    a = 10.0l;
+    eps = 1.0l;
+    while(eps > 0.00001l) {
+        b = a - f(a, d, p) / g(a, d);
+        eps = fabs(a - b);
+        a = b;
+    }
+
+    return a;
+}
+
+long double length(long double a, long double d)
+{
+    return 2.0l * a * sinh(d / (2.0l * a));
+}
+
+int main()
+{
+    long double a, d, l, p;
+
+    while (scanf("%Lf %Lf", &p, &d) == 2) {
+        a = newtonRaphson(d, p);
+        l = length(a, d);
+        cout.precision(3);
+        cout << fixed << l << endl;
+    }
+
+    return 0;
+}
+
