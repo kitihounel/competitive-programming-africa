c63d081fd6186f85bca03bebc61f15798994483e
kitihounel <kitihounel@gmail.com>
2018-07-08 01:56:44 +0100

    Fixed typo

--- south-africa/2013/isle.cpp
+++ south-africa/2013/isle.cpp
@@ -1,6 +1,6 @@
 /**
  * This problem asks to find the number of points in a convex hull.
- * Given the size of the input, it is better to use Grahan scan to build the convex hull.
+ * Given the size of the input, it is better to use Graham scan to build the convex hull.
  * Links for algorithms commonly used to build convex hull.
  *  - Jarvis algorithm: https://www.geeksforgeeks.org/convex-hull-set-1-jarviss-algorithm-or-wrapping/
  *  - Graham scan: https://www.geeksforgeeks.org/convex-hull-set-2-graham-scan/ 
