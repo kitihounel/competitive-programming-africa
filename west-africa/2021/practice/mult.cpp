67dd142d9a8702d5cfd43168254a15383a6c4c75
Kitihounel <kitihounel@gmail.com>
2021-01-25 07:39:00 +0100

    Fixed error in 'mult.cpp'

--- west-africa/2021/practice/mult.cpp
+++ west-africa/2021/practice/mult.cpp
@@ -4,31 +4,32 @@
 #include <iterator>
 
 using namespace std;
+using i64 = long long int;
 
 int main()
 {
     int t, n, k;
-    int a, p;
+    i64 a, p;
 
     ios::sync_with_stdio(false);
     scanf("%d", &t);
     while (t > 0) {
         scanf("%d %d", &n, &k);
 
-        vector<int> v(n);
+        vector<i64> v(n);
 
         for (int i = 0; i < n; ++i) {
-            scanf("%d", &a);
+            scanf("%lld", &a);
             v[i] = a;
         }
         sort(begin(v), end(v));
 
-        p = 1;
+        p = 1ll;
         for (int i = n - 1; k > 0; --i, --k) {
             p *= v[i];
         }
 
-        printf("%d\n", p);
+        printf("%lld\n", p);
         --t;
     }
 
